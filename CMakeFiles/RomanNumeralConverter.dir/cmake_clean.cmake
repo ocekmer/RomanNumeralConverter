bfd012a9c4b682e5a29e06a0ca84d7c123bb3d48
ocekmer <ozgurcekmer@gmail.com>
2020-01-30 21:17:17 -0800

    Added cmake files

---
+++ CMakeFiles/RomanNumeralConverter.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RomanNumeralConverter.dir/Driver.cpp.o"
+  "CMakeFiles/RomanNumeralConverter.dir/RomanNumbers.cpp.o"
+  "RomanNumeralConverter"
+  "RomanNumeralConverter.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/RomanNumeralConverter.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
